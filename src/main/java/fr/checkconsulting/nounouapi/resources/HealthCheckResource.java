5f1f5a98445ba7c4cd8ffb38111a384520cf4670
mahdi chekini <me.chekini@gmail.com>
2022-11-01 17:19:40 +0100

    Add health check resource

---
+++ src/main/java/fr/checkconsulting/nounouapi/resources/HealthCheckResource.java
@@ -0,0 +1,19 @@
+package fr.checkconsulting.nounouapi.resources;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/nounou")
+public class HealthCheckResource {
+
+
+
+    @GetMapping("health")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("application is UP");
+    }
+}
