4b221bf14999f4d6d94863a1f64efe9da958976a
mahdcheck <me.chekini@gmail.com>
2022-12-12 01:46:51 +0100

    replace Security configuration by old one

---
+++ src/main/java/fr/checkconsulting/nounouapi/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package fr.checkconsulting.nounouapi.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/health/").permitAll()
+                .antMatchers("/api/v1/search/**").permitAll()
+                .antMatchers("/api/v1/famille/**").authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
+}
+
