320e5657ce2c4917aa72e905b36f984deec4a7e7
Abderraouf SALAH <abderraouf.salah@intalio.com>
2022-12-12 23:29:56 +0100

    Correction de la déclaration des disponibilités

--- src/main/java/fr/checkconsulting/nounouapi/services/DisponibiliteService.java
+++ src/main/java/fr/checkconsulting/nounouapi/services/DisponibiliteService.java
@@ -29,7 +29,7 @@ public class DisponibiliteService {
         return disponibiliteRepository
                 .findAllByNounouId(nounouId)
                 .stream()
-                .map(this::mapToDisponibiliteDto)
+                .map(disponibilite -> modelMapper.map(disponibilite, DisponibiliteDTO.class))
                 .collect(Collectors.toList());
     }
 
@@ -39,41 +39,13 @@ public class DisponibiliteService {
         List<Disponibilite> result = disponibiliteRepository.saveAll(
                 listDisponibiliteDTO
                         .stream()
-                        .map(this::mapToDisponibilite)
+                        .map(disponibiliteDTO -> modelMapper.map(disponibiliteDTO, Disponibilite.class))
                         .collect(Collectors.toList())
         );
         return result
                 .stream()
-                .map(this::mapToDisponibiliteDto)
+                .map(disponibilite -> modelMapper.map(disponibilite, DisponibiliteDTO.class))
                 .collect(Collectors.toList());
-    }
-
-    private Disponibilite mapToDisponibilite(DisponibiliteDTO disponibiliteDTO){
-        return Disponibilite.builder()
-                .dateDebutMatin(disponibiliteDTO.getDate_debut_matin() != null ? disponibiliteDTO.getDate_debut_matin().toLocalTime() : null)
-                .dateFinMatin(disponibiliteDTO.getDate_fin_matin() != null ? disponibiliteDTO.getDate_fin_matin().toLocalTime() : null)
-                .dateDebutMidi(disponibiliteDTO.getDate_debut_midi() != null ? disponibiliteDTO.getDate_debut_midi().toLocalTime() : null)
-                .dateFinMidi(disponibiliteDTO.getDate_fin_midi() != null ? disponibiliteDTO.getDate_fin_midi().toLocalTime() : null)
-                .dateDebutSoir(disponibiliteDTO.getDate_debut_soir() != null ? disponibiliteDTO.getDate_debut_soir().toLocalTime() : null)
-                .dateFinSoir(disponibiliteDTO.getDate_fin_soir() != null ? disponibiliteDTO.getDate_fin_soir().toLocalTime() : null)
-                .jour(disponibiliteDTO.getJour())
-                .id(disponibiliteDTO.getId())
-                .nounouId(disponibiliteDTO.getNounouId())
-                .build();
-    }
-
-    private DisponibiliteDTO mapToDisponibiliteDto(Disponibilite disponibilite){
-        return DisponibiliteDTO.builder()
-                .date_debut_matin(disponibilite.getDateDebutMatin() != null ? disponibilite.getDateDebutMatin().atDate(LocalDate.now()) : null)
-                .date_fin_matin(disponibilite.getDateFinMatin() != null ? disponibilite.getDateFinMatin().atDate(LocalDate.now()) : null)
-                .date_debut_midi(disponibilite.getDateDebutMidi() != null ? disponibilite.getDateDebutMidi().atDate(LocalDate.now()) : null)
-                .date_fin_midi(disponibilite.getDateFinMidi() != null ? disponibilite.getDateFinMidi().atDate(LocalDate.now()) : null)
-                .date_debut_soir(disponibilite.getDateDebutSoir() != null ? disponibilite.getDateDebutSoir().atDate(LocalDate.now()) : null)
-                .date_fin_soir(disponibilite.getDateFinSoir() != null ? disponibilite.getDateFinSoir().atDate(LocalDate.now()) : null)
-                .nounouId(disponibilite.getNounouId())
-                .jour(disponibilite.getJour())
-                .id(disponibilite.getId())
-                .build();
     }
 
     @Transactional
